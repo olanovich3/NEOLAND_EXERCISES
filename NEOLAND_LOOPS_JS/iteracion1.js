409a1ccb854e6520e64d6093a0fc6b7f867eef20
Carlos Olano <carlos.olano@live.com>
2023-01-11 18:43:24 +0100

    iteracion1

---
+++ NEOLAND_LOOPS_JS/iteracion1.js
@@ -0,0 +1,15 @@
+//1
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+for (const item of products){
+
+    if(item.includes("Camiseta")){
+
+    console.log(item);
+
+    }
+
+   }
+
+     
+
