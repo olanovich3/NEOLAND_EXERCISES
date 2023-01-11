274c11c731cf486350cb71bffad1fa3454cadfed
Carlos Olano <carlos.olano@live.com>
2023-01-11 18:10:34 +0100

    iteraciones

---
+++ NEOLAND_BASICS_ALGORITHMS_JS/iteracion3.js
@@ -0,0 +1,25 @@
+//1.1
+let x=10;
+let y=5;
+let z=x*y;
+console.log(z);
+//1.2
+let a=10;
+let b=2;
+let c=a/b;
+console.log(c);
+//1.3
+let d=15;
+let e=9;
+let f=d%e;
+console.log(f)
+//1.4
+let p=10;
+let j=5;
+o=p+j;
+console.log(o);
+//1.5
+let u=10;
+let m=5;
+let i=c*m;
+console.log(i);
---
+++ NEOLAND_BASICS_ALGORITHMS_JS/iteracion6.js
@@ -0,0 +1,25 @@
+for (let i=0;i<10;i++){
+    console.log(i)
+}
+
+//1.2
+
+for (let z=0;z<10;z++){
+    if(z%2==0){
+        console.log(z);
+    }
+    
+}
+
+//1.3
+for (let y=0; y<11; y++){
+
+    if (y>=0 && y<10){
+
+        console.log('Intentando dormir 🐑');
+    }
+    else if (y==10){
+
+        console.log("Dormido");
+    }
+}
