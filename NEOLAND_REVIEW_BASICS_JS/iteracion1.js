2aaef2bd231c960bc4cdc3d7df796ea1e9a95caf
Carlos Olano <carlos.olano@live.com>
2023-01-17 20:00:01 +0100

    review basics

---
+++ NEOLAND_REVIEW_BASICS_JS/iteracion1.js
@@ -0,0 +1,29 @@
+// **Iteración #1: Mix for e includes**
+
+// Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función **.includes()**
+
+const movies = [
+  { title: "Madaraspar", duration: 192, categories: ["comedia", "aventura"] },
+  { title: "Spiderpan", duration: 122, categories: ["aventura", "acción"] },
+  {
+    title: "Solo en Whatsapp",
+    duration: 223,
+    categories: ["comedia", "thriller"],
+  },
+  {
+    title: "El gato con guantes",
+    duration: 111,
+    categories: ["comedia", "aventura", "animación"],
+  },
+];
+
+const categoriesKey = [];
+for (const type of movies) {
+  for (const key of type.categories) {
+    if (!categoriesKey.includes(key)) {
+      categoriesKey.push(key);
+    }
+  }
+}
+
+console.log(categoriesKey);
---
+++ NEOLAND_REVIEW_BASICS_JS/iteracion4.js
@@ -0,0 +1,13 @@
+// Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parametro. Haz varios ejemplos y compruebalos.
+
+// Sugerencia de función:
+const names = ["Caracol", "Mosquito", "Salamandra", "Ajolote"];
+
+const findArrayIndex = (list, word) => {
+  if (list.includes(word)) {
+    return list.indexof(word);
+  }
+};
+
+const x = list.includes(word);
+console.log(x);
